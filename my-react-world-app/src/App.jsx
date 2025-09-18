23e2b2d41bf6fc57197392459c6311a37890b95b
shams al labib <samslabib19@gmail.com>
2025-09-18 21:21:17 +0600

    added

---
+++ my-react-world-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Countries from './components/Countries/countries'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ my-react-world-app/src/components/Countries/countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>I'm connectedt mister</h1>
+        </div>
+    );
+};
+
+export default Countries;
